5ef7d688ef0904bb158d2922b6f243ce6de95883
unknown <cenkerdonmez@gmail.com>
2023-06-06 14:52:45 +0300

    done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import user from "../data/user";
 function ProjectItem({ name, about, technologies }) {
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((item)=><span key={item}>{item}</span>)}
       </div>
     </div>
   );
